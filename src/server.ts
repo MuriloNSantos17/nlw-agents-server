b5e54316f7caa044a66248dbef11aef9bb1e8ba7
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2025-07-08 23:15:33 -0300

    Commit add readme

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import fastify from "fastify";
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+
+import { fastifyCors } from '@fastify/cors'
+import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: 'http://localhost:5173'
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+
+app.get("/health", () => {
+    return "OK"
+})
+
+app.listen({ port: env.PORT }).then(() => {
+    console.log("Http server is running 🚀")
+})
+
+app.register(getRoomsRoute)
